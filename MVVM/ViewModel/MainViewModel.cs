428004b3e7b0baa0f6322fd4306a420b70f08141
kien <kienvtls@gmail.com>
2018-10-30 10:46:17 +0700

    1

---
+++ MVVM/ViewModel/MainViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using MVVM.VIEWMODEL;
+
+namespace MVVM.ViewModel
+{
+    public class MainViewModel: BaseViewModel
+    {
+        public MainViewModel()
+        {
+         //   MessageBox.Show("da vao datacontex mainviewmodel");
+        }
+    }
+}
